c80e09e41873460a46e37bc5f1c9266db487f0b7
github username <github email address>
2020-08-22 00:06:42 -0400

    Added nested route for comments to tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  resources :comments
-  resources :tasks
+  
+  resources :tasks do
+    resources :comments
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
