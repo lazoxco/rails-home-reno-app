58599d056c7e3fd3b1113bc3728465cbd9f74699
github username <github email address>
2020-08-23 16:10:09 -0400

    Fixed omniauth error.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.user OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '649c050759f8954aa818', '69fffea89d392db05f820e42eeee6e643b1b8760'
 end
